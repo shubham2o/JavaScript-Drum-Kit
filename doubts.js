ae2ce482e894ebd982f39bd1ecb454555a785af1
shubham2o <shubhamsharma.ha@gmail.com>
2022-03-29 08:27:54 +0530

    What does .play() Method do? added

--- doubts.js
+++ doubts.js
@@ -3,3 +3,15 @@ function showDetails(animal) {
     var animalType = animal.getAttribute("data-animal-type");
     alert("The " + animal.innerHTML + " is a " + animalType + ".");
 }
+
+
+// What does .play() Method do?
+var music = document.getElementById("myAudio");
+
+function playAudio() {
+    music.play();
+}
+
+function pauseAudio() {
+    music.pause();
+}
